0dc929b879c7a5aa58ad91acd642890af489aefd
moratojoan <moratojoan@gmail.com>
2018-11-14 18:19:54 +0100

    Start Json

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,17 @@
 var app = new Vue({
-    el:"#app",
-    data:{
-        pageToShow:"index2" 
+    el: "#app",
+    data: {
+        pageToShow: "index2"
     },
-    methods:{
-        showHidePages: function(id_pageToShow){
-            var allPages = document.getElementsByClassName("page");
-            for(let i=0; i<allPages.length;i++){
-                if(allPages[i].getAttribute("id") == id_pageToShow){
-                    this.pageToShow=id_pageToShow;
-                }
-            }
+    methods: {
+        showHidePages: function (id_pageToShow) {
+            //            var allPages = document.getElementsByClassName("page");
+            //            for(let i=0; i<allPages.length;i++){
+            //                if(allPages[i].getAttribute("id") == id_pageToShow){
+            //                    this.pageToShow=id_pageToShow;
+            //                }
+            //            }
+            this.pageToShow = id_pageToShow;
         }
     }
 })
